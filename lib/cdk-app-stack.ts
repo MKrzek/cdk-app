c1c63733593326bfae8d83d4373ace07c98e42d6
MKrzek <mkrzek@googlemail.com>
2021-06-11 14:20:13 +0100

    feat: add constructs to stack

--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -1,18 +1,50 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as cdk from '@aws-cdk/core';
+
+import * as lambda from "@aws-cdk/aws-lambda";
+import *  as cdk from '@aws-cdk/core';
+import * as apiGateway from '@aws-cdk/aws-apigateway';
+import * as s3 from "@aws-cdk/aws-s3";
+import *  as s3Notifications from "@aws-cdk/aws-s3-notifications";
+import { Duration } from "@aws-cdk/aws-cloudwatch/node_modules/@aws-cdk/core/lib/duration";
+import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
+import { ToDoBackend } from "./todo-backend";
+
+
+
+
 
 export class CdkAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkAppQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const toDoBackend = new ToDoBackend(this, 'ToDoBackend')
+
+
+    const helloLambda = new lambda.Function(this, 'HelloLambda', {
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      memorySize: 256,
+      timeout: Duration.seconds(30),
+      environment: {
+        isProduction: 'Not is not'
+      }
+    })
+    new apiGateway.LambdaRestApi(this, 'Endpoint', {
+      handler: helloLambda
+    })
+
+    const logoBucket = new s3.Bucket(this, 'LogoBucket', {
+      publicReadAccess: true
     });
 
-    const topic = new sns.Topic(this, 'CdkAppTopic');
+    logoBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3Notifications.LambdaDestination(helloLambda)
+    );
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new s3Deployment.BucketDeployment(this, "DeployLogo", {
+      destinationBucket: logoBucket,
+      sources: [s3Deployment.Source.asset("./assets")]
+    })
   }
 }
