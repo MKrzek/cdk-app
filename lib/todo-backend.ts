bb64531ee81845cde10e1311d4d7d730a26049b3
MKrzek <mkrzek@googlemail.com>
2021-06-11 14:22:23 +0100

    feat: add custom construct

---
+++ lib/todo-backend.ts
@@ -0,0 +1,7 @@
+import * as cdk from "@aws-cdk/core"
+
+export class ToDoBackend extends cdk.Construct {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id)
+    }
+}
